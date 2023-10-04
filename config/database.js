cb4194d326539f2dcde5b2b63363df8c726a9ed0
Hipparchus07 <ashwini.kumar787@gmail.com>
2023-10-05 02:57:45 +0530

    Db connection  updated

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
+const uri=process.env.DATABASE_URL;
 const dbConnect = () => {
   mongoose
-    .connect(process.env.DATABASE_URL, {
+    .connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex:true,
     })
     .then(() => console.log("connection to db established"))
     .catch((err) => {
